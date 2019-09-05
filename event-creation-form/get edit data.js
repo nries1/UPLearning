5b2e5cc0b5ee78c29ed4194f15bcde2c952d281f
nries1 <nries1@gmail.com>
2019-09-05 16:49:00 -0400

    setting up for redo of populate form function

--- event-creation-form/get edit data.js
+++ event-creation-form/get edit data.js
@@ -4,20 +4,20 @@ function getEditData(id,instance) {
   var s = ss.getSheetByName("event creation form responses");
   var data = s.getDataRange().getValues();
   var headers = data[0];
-  var obj = {bco: instance, "sessions": []};
+  var out = {success: true, form_values: [{bco: instance}, {sessions: []}]};
   for (var i=1;i<data.length;i++) {
     if (data[i][50]===id) {
       for (var j=0;j<52;j++) {
         if (data[i][j]!=="") {
-            obj[headers[j]]=data[i][j].toString();
+            var key = headers[j];
+            out.form_values.push({key: data[i][j].toString()})
         }
       }
       for (var k=58;k<69;k++) {
-        if (data[i][k]!=="") {obj.sessions.push(JSON.parse(data[i][k]))}
+        if (data[i][k]!=="") {out.form_values.sessions.push(JSON.parse(data[i][k]))}
       }
       break;
     }
   }
-  Logger.log(obj);
   return JSON.stringify(obj);
 }
