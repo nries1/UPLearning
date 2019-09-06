a2a46e220816b1ef6005c417d350e6ff24a5d3be
nries1 <nries1@gmail.com>
2019-09-06 13:16:31 -0400

    nm

--- event-creation-form/get edit data.js
+++ event-creation-form/get edit data.js
@@ -4,9 +4,11 @@ function getEditData(id,instance) {
   var s = ss.getSheetByName("event creation form responses");
   var data = s.getDataRange().getValues();
   var headers = data[0];
-  var out = {success: true, form_values: [{bco: instance}], sessions: []};
+  var out = {success: true, form_values: [{bco: instance}], sessions: [], 'Event Creator': '', 'TimeStamp': ''};
   for (var i = 1; i < data.length; i++) {
     if (data[i][50] === id) {
+      out['TimeStamp'] = data[i][0];
+      out['Event Creator'] = data[i][1]
       for (var j=0; j<52 ; j++) {
         if (data[i][j] !== "") {
             var formValue = {};
