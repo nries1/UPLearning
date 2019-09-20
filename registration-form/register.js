a6e599e68ff132a7a5c129aecd05b6bebccd1efc
Nicolas Ries <nries1@gmail.com>
2019-09-20 10:04:35 -0400

    fixing typo in waitlist confirmation

--- registration-form/register.js
+++ registration-form/register.js
@@ -4,7 +4,7 @@ function register(data,dbnList,instance) {
   var prefix = boMap[instance].prefix;
   if (data.edit) {return edit(data,dbnList,instance)}
   var regMsg;
-  data.waitlist ? regMsg = "You have been added to the waitlist. <a href='https://docs.google.com/document/d/1b0Y6upbnXdY9Dn0-NwZhE8v1e_sDYJvE7vSB2jiY1K0/edit?usp=sharing' target=_blank>Why am I on the waitlis?</a>" : regMsg = "You have been registered successfully"
+  data.waitlist ? regMsg = "You have been added to the waitlist. <a href='https://docs.google.com/document/d/1b0Y6upbnXdY9Dn0-NwZhE8v1e_sDYJvE7vSB2jiY1K0/edit?usp=sharing' target=_blank>Why am I on the waitlist?</a>" : regMsg = "You have been registered successfully"
   var emailSendStatus = emailConfirmation(data, instance);
   //Try to append this registrant's data to the database
   if (appendRegistrant(data, dbnList, instance, emailSendStatus)) {
