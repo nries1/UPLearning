fac71c77997014cfaa3afb2dfeacf0101ca310e4
theresasing14 <55796922+theresasing14@users.noreply.github.com>
2020-01-03 13:32:27 -0500

    Delete feedback.js

--- feedback.js
+++
@@ -1,4 +0,0 @@
-function createFbForm(eventName, id, sessionNumber) {
-  var url = "https://script.google.com/macros/s/AKfycbwXs13RmZjPTZMeTDLSNY6r5xVwmFc-HRGNW3lHFLRudH1iBG0/exec?title=" + eventName + "&id=" + id + "&num=" + sessionNumber;
-  return url;
-}
